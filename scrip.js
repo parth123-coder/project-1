d077f14b08695f7efaa1ee0ad8fbe2a04a3de35d
parth123-coder <parth.samdani@sangamschoolbhilwara.com>
2025-06-10 15:52:04 +0530

    Initial commit

---
+++ scrip.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize AOS animations
+    AOS.init({
+        delay: 700,
+        offset: 200,
+        duration: 1000,
+        once: true
+    });
+});
